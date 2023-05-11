1556a6c097061b62b08a95dca6a2d2ccace54115
Giorgio Lasala <salem84@users.noreply.github.com>
2023-05-11 20:34:06 +0200

    download artifacts

--- .devcontainer/pre-create.sh
+++ .devcontainer/pre-create.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+workflow_name="Run Analysis"
 
 mkdir $(pwd)/pgdata
 touch $(pwd)/pgdata/leggimi.txt 
+
+echo "Get info on latest GitHub Actions run..."
+run_id=$(gh run list -w "$workflow_name" -L 1 --json databaseId | jq '.[]| .databaseId')
+
+echo "GitHub Actions latest runId: $run_id"
+
+echo "Download artifacts..."
+gh run download $run_id -D $(pwd)/pgdata
+
+
+
