b3586cfc5f440d32316ca054d5eeb03ea1c8b810
Giorgio Lasala <salem84@users.noreply.github.com>
2023-05-13 22:04:01 +0000

    net version script

---
+++ queries/net-versions.sql
@@ -0,0 +1,15 @@
+SELECT
+  dependencies_net_version,
+  SUM(count) AS count
+FROM (SELECT
+  1 AS count,
+  COALESCE(
+    SUBSTRING(public.git_files.contents FROM '<TargetFramework[s]?>(.+)</TargetFramework[s]?>'),
+    SUBSTRING(public.git_files.contents FROM '<TargetFrameworkVersion>(.+)</TargetFrameworkVersion>')
+ ) AS dependencies_net_version
+  FROM public.git_files
+  INNER JOIN public.repos ON public.repos.id = public.git_files.repo_id
+  WHERE public.git_files.path LIKE '%csproj'
+) AS t
+WHERE dependencies_net_version IS NOT NULL
+GROUP BY dependencies_net_version
