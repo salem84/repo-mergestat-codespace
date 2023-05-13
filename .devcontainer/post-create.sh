d11f7989b13cc26501f9b39a767805eebc2310ce
Giorgio Lasala <salem84@users.noreply.github.com>
2023-05-13 21:43:49 +0000

    fix script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,7 +11,7 @@ gh run download $run_id -D $(pwd)/pgdata
 
 echo "Starting database restore..."
 touch ~/.pgpass
-echo "postgres:5432:mergestat:postgres:postgres > ~/.pgpass
+echo "postgres:5432:mergestat:postgres:postgres" > ~/.pgpass
 chmod 0600 ~/.pgpass
 
-pg_restore -h postgres -U postgres -d mergestat --clean -x $(pwd)/pgdata/pg-dump/backup_mergestat.dump 
+pg_restore -h postgres -U postgres -d mergestat --clean -x --no-owner $(pwd)/pgdata/pg-dump/backup_mergestat.dump 
